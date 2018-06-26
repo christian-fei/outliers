46f62620eecff02650d9616ca9b871d95e4e0ce7
Christian Fei <crifei93@gmail.com>
2018-06-26 17:37:44 +0200

    update litecoin june

--- test/outlier-detection.test.js
+++ test/outlier-detection.test.js
@@ -7,11 +7,31 @@ const litecoinJune = require('../samples/litecoin-june.json')
 test('outliers in litecoin june', () => {
   const lows = litecoinJune.map(l => +l.low)
   const rreal = outliers(lows)
-  assert.deepEqual(rreal, [118.95, 119.62, 122.37])
+
+  assert.deepEqual(rreal, [ 74.29,
+    117.12,
+    117.74,
+    118.56,
+    118.7,
+    118.73,
+    118.95,
+    119.62,
+    122.37 ])
 })
 test('outliers in litecoin june with custom divider', () => {
   const divider = 2
   const lows = litecoinJune.map(l => +l.low)
   const rreal = outliers(lows, divider)
-  assert.deepEqual(rreal, [118.56, 118.7, 118.73, 118.95, 119.62, 122.37])
+
+  assert.deepEqual(rreal, [ 74.29,
+    79.02,
+    116.44,
+    117.12,
+    117.74,
+    118.56,
+    118.7,
+    118.73,
+    118.95,
+    119.62,
+    122.37 ])
 })
