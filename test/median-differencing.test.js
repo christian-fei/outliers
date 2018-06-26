8769384d63eb0538724939617c932186c98262e0
Christian Fei <crifei93@gmail.com>
2018-06-26 17:12:36 +0200

    add test for indexes

--- test/median-differencing.test.js
+++ test/median-differencing.test.js
@@ -8,4 +8,6 @@ test('medianDifferencing in litecoin june', () => {
   const lows = litecoinJune.map(l => +l.low).sort((a, b) => new Date(a.time) - new Date(b.time))
   const rreal = medianDifferencing(lows)
   assert.deepEqual(rreal, [ 117.12, 96.36, 81.4, 104.12, 117.74, 99.37 ])
+  const rrealIndexes = medianDifferencing(lows, {indexes: true})
+  assert.deepEqual(rrealIndexes, [ 1, 2, 11, 12, 14, 22 ])
 })
